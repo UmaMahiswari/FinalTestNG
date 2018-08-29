9cf1a922d01a51ecc22067515929577ba2d5ae62
IBM <umamahiswari.r@gmail.com>
2018-08-30 03:57:49 +0530

    Commit 1

---
+++ com.tdd/src/test/java/Runner/Test_Runner.java
@@ -0,0 +1,22 @@
+package Runner;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Test_Runner {
+@Test
+public void test() 
+{
+	System.out.println("");
+	}
+@BeforeTest
+public void beforeTest()
+{
+	System.out.println("");
+}
+@AfterTest
+public void afterTest(){
+	System.out.println("");
+}
+}
